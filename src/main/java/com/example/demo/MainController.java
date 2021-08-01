718ac85dc39836f84fca3f454add7f7aabf548ab
Uzeen <41296316+Uzeen@users.noreply.github.com>
2021-08-02 01:14:48 +0530

    Update MainController.java

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,1 +1,12 @@
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class EmployeeController {
+
+  @GetMapping("/employees")
+  String all() {
+    return "Hello World";
+  }
+}
